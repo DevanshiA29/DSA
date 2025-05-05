76f033689edc7e4700801a59b909ec0dafc852f5
Devanshi Awasthi <156073050+DevanshiA29@users.noreply.github.com>
2025-05-05 19:17:30 +0530

    Add files via upload
    dive into nodes, pointers & unstoppable problem-solving!

---
+++ Palindrome.java
@@ -0,0 +1,51 @@
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Palindrome {
+    public class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        }
+    // find midElement
+    ListNode findmid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = findmid(head);
+        ListNode curr = mid;
+        ListNode next = null, prev = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        mid = prev;
+        while (mid != null) {
+            if (head.val == mid.val) {
+                head = head.next;
+                mid = mid.next;
+            } else
+                return false;
+        }
+        return true;
+    }
+}
